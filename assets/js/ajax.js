a7172560b0ddf1e09a82572f7dd1968cdff5a3ae
Ali Hasnain <abdulrehman76261@gmail.com>
2023-01-04 11:40:30 -0800

    like dislike complete exercise with wpacdemy

---
+++ assets/js/ajax.js
@@ -0,0 +1,17 @@
+function wpac_like_btn_ajax(postId,usrid) {
+    
+	var post_id = postId;
+    var usr_ID = usrid;
+	jQuery.ajax({
+		url : wpac_ajax_url.ajax_url,
+		type : 'post',
+		data : {
+			action : 'wpac_like_btn_ajax_action',
+			pid : post_id,
+			uid : usr_ID
+		},
+		success : function( response ) {
+            jQuery("#wpacAjaxResponse span").html(response);
+		}
+	});
+}
